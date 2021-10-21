07d8d107ec7f33ffa9adb8c9e0d35c1ac27611ba
Dobrosław Żybort <dobroslaw.zybort@fujitsu.com>
2021-10-21 14:33:03 +0200

    Init

---
+++ main.go
@@ -0,0 +1,72 @@
+// go-swagger example
+//
+// Base go-swagger
+//
+//     	Schemes: http
+//     	BasePath: /api
+//     	Version: 0.1.0
+//
+//     	Consumes:
+//     	- application/json
+//     	Produces:
+//     	- application/json
+//
+//
+// swagger:meta
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hyperledger/fabric-sdk-go/pkg/client/msp"
+
+	_ "github.com/matrixik/swag-perf/docs"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+// @title swaggo example
+// @version 0.1.0
+// @description.markdown
+// @BasePath /api
+func main() {
+	r := gin.Default()
+	api := r.Group("/api")
+	{
+		api.GET("", mainHandler)
+	}
+
+	r.Static("/swagger-ui", "swagger-ui/")
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	r.Run(":8080")
+}
+
+// swagger:model mspIdentityResponse
+type _ struct {
+	// Response with identity data
+	// in: body
+	msp.IdentityResponse
+}
+
+// swagger:route GET / example
+//
+// go-swagger docs summary
+//
+// go-swagger docs description
+//
+// responses:
+//   200: mspIdentityResponse
+//
+// @Summary swaggo docs summary
+// @Description swaggo docs description
+// @Success 200 {object} msp.IdentityResponse
+// @Produce json
+// @Router / [get]
+// @T
+func mainHandler(c *gin.Context) {
+	resp := msp.IdentityResponse{}
+	c.JSON(http.StatusOK, resp)
+}
